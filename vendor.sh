e38d91c283b3aa11d108151a38d4da4c2fcf5af0
Abdo <abd.nh25@gmail.com>
2022-10-01 04:41:24 +0300

    Fix pip command in vendor.sh

--- vendor.sh
+++ vendor.sh
@@ -1,4 +1,4 @@
-pip install https://github.com/abdnh/zimply-core@eddf548b778d9046020ba84d9912257d1cb949fb -t src/vendor
+pip install git+https://github.com/abdnh/zimply-core@eddf548b778d9046020ba84d9912257d1cb949fb -t src/vendor
 # TODO: make spacy optional
 pip install spacy==3.4.1 -t src/vendor
 python -m spacy download el_core_news_sm -t src/vendor
