2276dcec7accc69d3ea7ad626368f37d3cd74ede
Abdo <abd.nh25@gmail.com>
2022-10-02 15:26:02 +0300

    Try installing python-zstandard from source
    The previous commit didn't work. Building from source works for me on a
    normal Ubuntu installation.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,3 +1,12 @@
 pip install git+https://github.com/abdnh/ankibuild@5092188f6b7c73e7bab2c7f64e7008a7e24466f6#egg=ankibuild[qt5,qt6]
-make vendor
+
+# Build zstandard from source to work around "C API versions mismatch" error
+git clone https://github.com/indygreg/python-zstandard
+cd python-zstandard
+git checkout 28cd74decb47d8a5294eb59e793cb24a0f242e9e
+cd ..
+pip install ./python-zstandard/ -t src/vendor
+
+pip install -U git+https://github.com/abdnh/zimply-core@eddf548b778d9046020ba84d9912257d1cb949fb -t src/vendor
+
 make
