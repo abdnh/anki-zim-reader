ff3ea5b621b5afce496a2c0416d9afb9c0122c51
Abdo <abd.nh25@gmail.com>
2022-10-02 14:49:10 +0300

    Install & update zstandard explicitly
    The build produced by GitHub actions is not working for me on Ubuntu due
    to an error like this:
    ```
    ImportError: zstd C API versions mismatch
    ```
    
    As python-zstandard bundles its own copy of zstandard, this mismatch
    should not occur. I suspect it's caused by a system version of
    python-zstandard (with debundled zstandard) getting in the way, so we force an upgrade here.

--- vendor.sh
+++ vendor.sh
@@ -6,8 +6,8 @@ while getopts ":s" option; do
          ;;
    esac
 done
-
-pip install git+https://github.com/abdnh/zimply-core@eddf548b778d9046020ba84d9912257d1cb949fb -t src/vendor
+pip install --force-reinstall zstandard
+pip install -U git+https://github.com/abdnh/zimply-core@eddf548b778d9046020ba84d9912257d1cb949fb -t src/vendor
 if [ "$spacy" = true ]; then
     pip install spacy==3.4.1 -t src/vendor
     python -m spacy download el_core_news_sm -t src/vendor
