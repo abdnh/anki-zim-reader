53c32d6e57101f6ec12a4120bc75be475be53dcf
Abdo <abd.nh25@gmail.com>
2022-10-03 03:55:42 +0300

    Try to vendor a cross-platform pyzstd distribution

---
+++ scripts/setup_pyzstd.sh
@@ -0,0 +1,38 @@
+# TODO: handle all supported Python versions
+
+pushd build
+
+platforms=(
+    win_amd64
+    manylinux2014_x86_64
+    manylinux2014_aarch64
+    macosx_10_9_x86_64
+    # FIXME: the arm64 shared library has the same name as the x86_64 one (_zstd.cpython-39-darwin.so)
+    # How to handle such situation?
+    # macosx_11_0_arm64
+)
+
+# Download wheels
+for platform in ${platforms[@]}; do
+    echo $platform
+    pip download pyzstd==0.15.3 --only-binary=:all: --python-version 39 --implementation cp --platform $platform
+done
+
+# Create a shared wheel from an arbitrary platform-specific wheel
+cp pyzstd-0.15.3-cp39-cp39-win_amd64.whl pyzstd.whl
+
+# Unzip wheels
+for wheel in *.whl; do
+    mkdir -p "${wheel%.*}"
+    pushd "${wheel%.*}"
+    unzip -o ../$wheel
+    popd
+done
+
+# Copy platform-specific library files to the shared wheel
+for dir in pyzstd-*/; do
+    cp $dir/pyzstd/c/_zstd* pyzstd/pyzstd/c/
+done
+
+# Copy to vendor dir
+cp -r ./pyzstd/* ../src/vendor
--- vendor.sh
+++ vendor.sh
@@ -6,7 +6,8 @@ while getopts ":s" option; do
          ;;
    esac
 done
-pip install -U git+https://github.com/abdnh/zimply-core@f682cedd8c774da2c492203e2ab75f6b7228a92c -t src/vendor
+pip install -U --no-deps git+https://github.com/abdnh/zimply-core@f682cedd8c774da2c492203e2ab75f6b7228a92c -t src/vendor
+./scripts/setup_pyzstd.sh
 if [ "$spacy" = true ]; then
     pip install spacy==3.4.1 -t src/vendor
     python -m spacy download el_core_news_sm -t src/vendor
