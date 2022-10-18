c2c55c782e1b6d053ae8fefd89fad22bc921b88b
Abdo <abd.nh25@gmail.com>
2022-10-18 22:38:03 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname: str) -> str:
 
 setup(
     name="zim_reader",
-    version="0.0.3",
+    version="0.0.4",
     description="ZIM file reader for Anki",
     long_description=read("README.md"),
     author="Abdo",
