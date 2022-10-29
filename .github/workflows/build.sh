1f150a2922d287ab7c623a7730b8f2a8107e855f
Abdo <abd.nh25@gmail.com>
2022-10-30 01:57:33 +0300

    Set min_point_version correctly for each package

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-pip install git+https://github.com/abdnh/ankibuild@5e346f5ddab5c783dfc35a16a1e8a5fdc7c7bed4#egg=ankibuild[qt5,qt6]
+pip install git+https://github.com/abdnh/ankibuild@d986a15c247981eccad651dd7f769331ea1092ea#egg=ankibuild[qt5,qt6]
 
 # These are the Python versions used in Anki versions we support (2.1.46 to 2.1.54+)
 python_versions=(
     38
     39
 )
+anki_min_versions=(
+    46
+    50
+)
+
 ./scripts/vendor_no_platform.sh
-for python_version in ${python_versions[@]}; do
+for i in $(seq 0 $(expr ${#python_versions[@]} - 1)); do
+    echo $i
+    python_version=${python_versions[$i]}
+    anki_min_version=${anki_min_versions[$i]}
     ./scripts/vendor_pyzstd.sh $python_version
     ./scripts/vendor_libzim.sh $python_version
-    # FIXME: min_point_version in manifest.json should depend on the Python version
-    make zip EXTRA_ARGS="--out build/zim_reader-py$python_version.ankiaddon"
-    make ankiweb EXTRA_ARGS="--out build/zim_reader-py$python_version-ankiweb.ankiaddon"
+    make zip EXTRA_ARGS="--out build/zim_reader-py$python_version.ankiaddon --manifest \"{\\\"min_point_version\\\": $anki_min_version}\""
+    make ankiweb EXTRA_ARGS="--out build/zim_reader-py$python_version-ankiweb.ankiaddon --manifest \"{\\\"min_point_version\\\": $anki_min_version}\""
     rm -rf build/pyzstd/
     rm -rf src/vendor/pyzstd*
     rm -rf build/libzim/
