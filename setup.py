0a0ff6f1985b1873191e15a961550a84a5bb4424
Abdo <abdo@abdnh.net>
2023-08-21 06:06:14 +0300

    Move py.typed to root
    This seems to work.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email="abd.nh25@gmail.com",
     url="https://github.com/abdnh/anki-zim-reader",
     package_dir={"zim_reader": "src"},
-    package_data={"zim_reader": ["py.typed"]},
+    package_data={"zim_reader": ["../py.typed"]},
     packages=["zim_reader", "zim_reader.dictionaries"],
     keywords="anki addon zim dictionary",
     install_requires=[
